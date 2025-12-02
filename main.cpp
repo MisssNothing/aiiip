fbce796d011757be85cd88434f8f12fc5b01e4fb
petrenko.esmira <esmira3942@gmail.com>
2025-12-02 09:37:56 +0300

    real first commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-
-struct IDraw {
-  virtual p_t begin() const = 0;
-  virtual p_t next(p_t p) const = 0;
-  virtual aIDraw() = default;
-};
-
+namespace eee {
+  struct p_t {
+    int x, y;
+  };
+  bool operator==(p_t a, p_t b);
+  bool operator!=(p_t a, p_t b);
+}
+int main()
+{
+  using namespace eee;
+  p_t a{1, 1}, b{0, 1};
+  std::cout << (a == b) << '\n';
+}
+bool eee::operator==(p_t a, p_t b) {
+  return a.x == b.x && a.y == b.y;
+}
+bool eee::operator!=(p_t a, p_t b) {
+  return !(a == b);
+}

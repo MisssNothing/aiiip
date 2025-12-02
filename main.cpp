a3b16f6a72bd2eea1cb284d5b60244ddedb80d4d
petrenko.esmira <esmira3942@gmail.com>
2025-12-02 09:56:01 +0300

    be be be

--- main.cpp
+++ main.cpp
@@ -13,6 +13,11 @@ namespace eee {
     virtual p_t next(p_t prev) const = 0;
     virtual ~IDraw() = default;
   };
+  struct Dot: IDraw {
+    p_t begin() const override;
+    p_t next(p_t prev) const override;
+    p_t d;
+  };
 }
 int main()
 {
@@ -20,9 +25,20 @@ int main()
   p_t a{1, 1}, b{0, 1};
   std::cout << (a == b) << '\n';
 }
+eee:p_t eee::Dot::begin() const {
+  return d;
+}
+eee:p_t eee::Dot::next(p_t prev) const {
+  if (prev != d) {
+    throw std::logic_error("bad prev");
+  }
+  return d;
+}
 bool eee::operator==(p_t a, p_t b) {
   return a.x == b.x && a.y == b.y;
 }
 bool eee::operator!=(p_t a, p_t b) {
   return !(a == b);
 }
+
+
